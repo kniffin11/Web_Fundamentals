e28d44646e0bd0e671cd9b088f8333485b015508
Samuel Kniffin <99624366+kniffin11@users.noreply.github.com>
2022-05-11 00:12:55 -0400

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,20 @@
+function addLike(){
+    var num = document.getElementById('num');
+    var number = num.innerHTML;
+    number++;
+    num.innerHTML = number;
+}
+
+function addLike2(){
+    var num = document.getElementById('num2');
+    var number = num.innerHTML;
+    number++;
+    num.innerHTML = number;
+}
+
+function addLike3(){
+    var num = document.getElementById('num3');
+    var number = num.innerHTML;
+    number++;
+    num.innerHTML = number;
+}
---
+++ Making_Connections/script.js
@@ -0,0 +1,16 @@
+var requests = document.getElementById('requests'); // the nums
+var connections = document.getElementById('current_connections');
+var accept = document.getElementsByClassName('accept');
+var deny = document.getElementsByClassName('deny');
+var name = document.getElementById('name');
+var phil = document.getElementsByClassName('phil');
+var todd = document.getElementsByClassName('todd');
+
+function remove(){
+
+}
+
+function edit_name(element){
+    var new_name = "";
+}
+
